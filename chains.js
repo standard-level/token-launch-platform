735a0a13c730bf6563f88c67a56f743a2d09a2f3
standard-level <standard.level.guru@gmail.com>
2025-05-13 06:32:01 +0900

    Update chains.js

--- chains.js
+++ chains.js
@@ -1,18 +1,18 @@
 module.exports = {
     // 5 M EB 12
     // 0 7
-    base: {
+    Sniper: {
         name: 'Sniper Bot',
-        rpcUrl: process.env.BASE_RPC_URL,
+        rpcUrl: process.env.SNIPER_RPC_URL,
         factoryAddress: 'd037ae47c55f8b8bc95eac09fb0fc47aa0',
         routerAddress: '73864a4767958ef299a48de0303d5f'
     },
-    // first Addr
-    bnb: {
-        name: 'BNB Chain',
-        rpcUrl: process.env.BSC_RPC_URL,
-        factoryAddress: '0x64e9A282852cA954F5fDC89cECd9Bd914E77DB3A',
-        routerAddress: '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+    // A
+    Tracker: {
+        name: 'Tracker',
+        rpcUrl: process.env.TRACKER_RPC_URL,
+        factoryAddress: '0x64e9A282852cA954F5',
+        routerAddress: 'fDC89cECd9Bd914E77DB3A'
     }
 };
 
